1009a5bcb374cdca1be8a18be7dd7be72c5c0a20
omelette-bio <omelettebio@proton.me>
2025-02-27 10:49:11 +0100

    debut du taff pour amd

--- bench/libpfm/data.py
+++ bench/libpfm/data.py
@@ -3,7 +3,7 @@
 events = ["CPU_CYCLES", "INSTRUCTIONS", "TIME_TAKEN(ns)", "L1_CACHE_LOADS", "L1_CACHE_LOAD_MISSES", "L1_CACHE_STORES", "L1_CACHE_STORE_MISSES"]
 data = dict()
 
-f = open('memory_bound.data','r')
+f = open('cpu_bound_amd.data','r')
 for i in f.readlines():
     words = i.split()
     if len(words) > 0 and words[0][:-1] in events:
@@ -21,4 +21,6 @@ for i in f.readlines():
 
 
 for key in data:
+    if key == "TIME_TAKEN(ns)":
+        print("FLOPNS", 500000/(data[key][0]/data[key][1]))
     print(key, data[key][0]/data[key][1])
